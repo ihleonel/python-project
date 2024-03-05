5bb8dc6d88c97b6c6119fe1bd76bd7b108dc84ec
MauricioVillca <157074580+hernanvillca@users.noreply.github.com>
2024-03-04 23:34:27 -0300

    Add files via upload

---
+++ ejercicio14.py
@@ -0,0 +1,12 @@
+#Crear una función que genere una lista de numeros entros aleatorios de 1000 elementos.
+# Importar y usar la función creada en el punto anterior
+import ejercicio13
+def generar_lista():
+    lista = list()
+    for i in range(1000):
+        lista.append(ejercicio13.random_x())
+    return lista
+
+print(" ---- GENERAR LISTA DE NUMEROS ALEATORIOS ----")
+numeros = generar_lista()
+print(numeros)
