c063896aa1c7726c928d14c6bb3c4d09c5553bcd
Mauricio Villca <hernanmauriciovillca32@outlook.com>
2024-03-23 21:02:29 -0300

    add exercise 18

---
+++ ejercicio18.py
@@ -0,0 +1,8 @@
+#Crear una funcion que genere una matriz cuadrada de 10 filas y 10 columnas de números aleatorios.
+#Importar y usar la funcion creada en el punto 14.
+import ejercicio14
+def generar_matriz(lista):
+	return [lista[(ind*10)-10:ind*10] for ind in range(1,11)] 
+	#ind = "indice" Se recorre la lista de a 10 elemento para generar la matriz  
+matriz = generar_matriz(ejercicio14.generar_lista())
+print(matriz)
