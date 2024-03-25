0917f20cf8569846cfe9d63ce953383501ed038c
Mauricio Villca <hernanmauriciovillca32@outlook.com>
2024-03-25 01:35:55 -0300

    exercise 18 modified so as not to print

--- ejercicio18.py
+++ ejercicio18.py
@@ -4,5 +4,5 @@ import ejercicio14
 def generar_matriz(lista):
 	return [lista[(ind*10)-10:ind*10] for ind in range(1,11)] 
 	#ind = "indice" Se recorre la lista de a 10 elemento para generar la matriz  
-matriz = generar_matriz(ejercicio14.generar_lista())
-print(matriz)
+#matriz = generar_matriz(ejercicio14.generar_lista())
+#print(matriz)
