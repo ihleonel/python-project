6acc80c6abe1f2e9abf63658c7b7157b0344a2aa
Mauricio Villca <hernanmauriciovillca32@outlook.com>
2024-04-24 02:53:34 -0300

    subiendo ejercicio 25

---
+++ ejercicio25.py
@@ -0,0 +1,25 @@
+# Importando y usando la clase "Usuario". Crear la clase "CreadorUsuario" que contenga:
+# Un metodo "crear_usuario" que reciba como parametro un objeto "Usuario" y 
+# que guarde los datos de ese objeto en el archivo "database.txt".
+# No debera sobreescribir o borrar los datos que ya existen en el archivo,
+# el nuevo usuario debera agregarse al final del archivo.
+from ejercicio23 import Usuario
+class CreadorUsuario():
+    def crear_usuario(self, Usuario_nuevo: Usuario):
+        archivo = open("database.txt", "a")
+        nombre = Usuario_nuevo.nombre
+        clave = Usuario_nuevo.clave
+        archivo.write(nombre + "," + clave)
+        archivo = open("database.txt", "r")
+        print(archivo.read())
+        archivo.close()
+
+if __name__ == '__main__':
+    nuevo_usuario = Usuario("Mauricio2024", "Ky4&Wc6%")
+    modificar = CreadorUsuario()
+    #modificar.crear_usuario(nuevo_usuario)
+    modificar.replace("Mauricio2024,Ky4&Wc6%")
+
+
+
+
