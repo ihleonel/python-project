a180bee7f937b0d225dbde553dc008e3ba405a62
Mauricio Villca <hernanmauriciovillca32@outlook.com>
2024-04-26 01:12:08 -0300

    agregado del ejercicio 26

---
+++ ejercicio26.py
@@ -0,0 +1,13 @@
+# Se deberá crear una función que reciba dos matrices cuadradas como parametros 
+# y debara retornar si son equivalentes o no. 
+# Para determinar si la funcion es correcta se debera correr el test python3 test_ejercicio26.py
+# y se deberá mostrar el mensaje "PRUEBAS PASADAS CON EXITO".
+def comparar_matrices_cuadradas(matriz_1, matriz_2):
+    if len(matriz_1) == len(matriz_2):
+        band = True
+        for i in range(len(matriz_1)):
+            if matriz_1[i] != matriz_2[i]:
+                band = False
+        return band
+    else :
+        return False
