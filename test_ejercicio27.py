24371c11b68c8ff6fa8cd3e1b7eabade5bcbdd8b
ihleonel <ibarra.h.leonel@gmail.com>
2024-04-28 14:26:22 -0300

    Fix: Agregando columna de comparacion en los test

--- test_ejercicio27.py
+++ test_ejercicio27.py
@@ -9,7 +9,8 @@ caso1 = [
      [3, 3, 3]],
     [[3, 2, 1],
      [3, 2, 1],
-     [3, 2, 1]]
+     [3, 2, 1]],
+    True
 ]
 caso2 = [
     [[1, 1, 1],
@@ -17,7 +18,8 @@ caso2 = [
      [3, 3, 3]],
     [[1, 2, 3],
      [1, 2, 3],
-     [1, 2, 3]]
+     [1, 2, 3]],
+    False
 ]
 caso3 = [
     [[1, 4, 2],
@@ -25,12 +27,16 @@ caso3 = [
      [9, 2, 3]],
     [[9, 3, 1],
      [2, 5, 4],
-     [3, 9, 2]]
+     [3, 9, 2]],
+    True
 ]
 
-if (comparar_matrices_cuadradas(rotar_matriz_cuadrada(caso1[0]), caso1[1]) and
-    comparar_matrices_cuadradas(rotar_matriz_cuadrada(caso2[0]), caso2[1]) and
-    comparar_matrices_cuadradas(rotar_matriz_cuadrada(caso3[0]), caso3[1])) :
+if (comparar_matrices_cuadradas(
+        rotar_matriz_cuadrada(caso1[0]), caso1[1]) == caso1[2] and
+    comparar_matrices_cuadradas(
+        rotar_matriz_cuadrada(caso2[0]), caso2[1]) == caso2[2] and
+    comparar_matrices_cuadradas(
+        rotar_matriz_cuadrada(caso3[0]), caso3[1]) == caso3[2]):
 
     print('PRUEBAS PASADAS CON EXITO')
 else:
