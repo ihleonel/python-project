e4cccc8b73ca446598678a22bcb622a364c5d215
ihleonel <ibarra.h.leonel@gmail.com>
2024-05-03 22:37:59 -0300

    Agregando ejemplo de clausula de guarda

--- ejercicio26.py
+++ ejercicio26.py
@@ -1,5 +1,5 @@
-# Se deberá crear una función que reciba dos matrices cuadradas como parametros 
-# y debara retornar si son equivalentes o no. 
+# Se deberá crear una función que reciba dos matrices cuadradas como parametros
+# y debara retornar si son equivalentes o no.
 # Para determinar si la funcion es correcta se debera correr el test python3 test_ejercicio26.py
 # y se deberá mostrar el mensaje "PRUEBAS PASADAS CON EXITO".
 def comparar_matrices_cuadradas(matriz_1, matriz_2):
@@ -9,5 +9,17 @@ def comparar_matrices_cuadradas(matriz_1, matriz_2):
             if matriz_1[i] != matriz_2[i]:
                 band = False
         return band
-    else :
+    else:
         return False
+
+
+# Clasulas de guarda
+# def comparar_matrices_cuadradas(matriz_1, matriz_2):
+#     if len(matriz_1) != len(matriz_2):
+#         return False
+
+#     band = True
+#     for i in range(len(matriz_1)):
+#         if matriz_1[i] != matriz_2[i]:
+#             band = False
+#     return band
